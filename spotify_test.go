8f931822e7b02ca7954e3c4ec1bc47b78789f4d9
Ali Hamidi <ahamidi@me.com>
2015-04-01 17:49:23 -0700

    initial attempt at using libspotify

---
+++ spotify_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/op/go-libspotify/spotify"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestSpotifyPlayTrack(t *testing.T) {
+
+	audio, _ := newAudioWriter()
+
+	session := newSpotifySession(audio)
+
+	credentials := spotify.Credentials{
+		Username: os.Getenv("SPOTIFY_USERNAME"),
+		Password: os.Getenv("SPOTIFY_PASSWORD"),
+	}
+	if err := session.Login(credentials, false); err != nil {
+		log.Fatal(err)
+	}
+
+	uri := "spotify:track:4yoirlyne2EwkftLG7CpvN"
+
+	link, err := session.ParseLink(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	track, err := link.Track()
+	if err != nil {
+		log.Fatal(err)
+	}
+	track.Wait()
+
+	player := session.Player()
+	if err := player.Load(track); err != nil {
+		fmt.Println("%#v", err)
+		log.Fatal(err)
+	}
+
+	player.Play()
+
+	assert.NotNil(t, track)
+	assert.Nil(t, err)
+}
